50b40c9477062c640dae23b2cb45ee08b89f6c66
Sanjana-cell <sanjanakr97@gmail.com>
2020-07-18 08:51:12 +0530

    Refactor: modified code to ensure if its tie then the game continues till the difference of minimum 2 points is achieved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -11,27 +11,36 @@ do
 	coin=$(($RANDOM%2))
 	if [ $coin -eq 1 ]
 	then
-		echo "HEADS IS WINNER"
+		echo "HEADS WON "$counter "game"
 		((counter++))
 		total_heads=$counter
 	else
-		echo "TAILS IS WINNER"
+		echo "TAILS WON "$counter "game"
 		((counter++))
 		total_tails=$counter
 	fi
+	 if [ $total_heads -ge 21 -o $total_tails -ge 21 ]
+        then
+                diff=$(($total_heads-$total_tails))
+                diff_1=$(($total_tails-$total_heads))
+                if [ $diff -ge 2 -o $diff_1 -ge 2 ]
+                then
+                        break
+                else
+                        ((limit++))
+                        continue
+                fi
+        fi
 done
-if [ $total_heads -eq $total_tails ]
+if [ $total_heads -eq $counter ]
 then
-	echo "It is a tie"
+	diff=$(($total_heads-$total_tails))
+	echo "HEAD IS OVER ALL WINNER BY " $diff 
 else
-	if [ $total_heads -eq $counter ]
-	then
-		diff=$(($total_heads-$total_tails))
-		echo "HEAD IS OVER ALL WINNER BY " $diff 
-	else
-		diff=$(($total_tails-$total_heads))  
-		echo "TAIL IS OVER ALL WINNER BY" $diff
-	fi
+	diff=$(($total_tails-$total_heads))  
+	echo "TAIL IS OVER ALL WINNER BY" $diff
 fi
+echo "heads" $total_heads
+echo "tails" $total_tails
 
 
