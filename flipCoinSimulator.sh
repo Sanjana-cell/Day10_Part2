6be36f5dc35186fb9e8e213a567d7ed2ae86fd78
Sanjana-cell <sanjanakr97@gmail.com>
2020-07-18 08:34:50 +0530

    Refactor: modified code to to continue till either ofthem have won 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,21 +5,33 @@ coin=0
 counter=0
 total_heads=0
 total_tails=0
-
-read -p "Enter how many times to flip the coin " flip
-while [ $counter -le $flip ]
+limit=21
+while [ $counter -le $limit ]
 do
 	coin=$(($RANDOM%2))
 	if [ $coin -eq 1 ]
 	then
 		echo "HEADS IS WINNER"
-		((total_heads++))
+		((counter++))
+		total_heads=$counter
 	else
 		echo "TAILS IS WINNER"
-		((total_tails++))
+		((counter++))
+		total_tails=$counter
 	fi
-	((counter++))
 done
+if [ $total_heads -eq $total_tails ]
+then
+	echo "It is a tie"
+else
+	if [ $total_heads -eq $counter ]
+	then
+		diff=$(($total_heads-$total_tails))
+		echo "HEAD IS OVER ALL WINNER BY " $diff 
+	else
+		diff=$(($total_tails-$total_heads))  
+		echo "TAIL IS OVER ALL WINNER BY" $diff
+	fi
+fi
 
-echo "HEADS WON " $total_heads " times"
-echo "TAILS WON " $total_tails " times"
+
