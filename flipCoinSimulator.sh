52930cd152970e250fdeaa4946849e06cbd97563
Sanjana-cell <sanjanakr97@gmail.com>
2020-07-18 08:24:08 +0530

    Refactor: added display statement to show no of times heads and tails won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,6 +3,8 @@
 #varibles
 coin=0
 counter=0
+total_heads=0
+total_tails=0
 
 read -p "Enter how many times to flip the coin " flip
 while [ $counter -le $flip ]
@@ -11,8 +13,13 @@ do
 	if [ $coin -eq 1 ]
 	then
 		echo "HEADS IS WINNER"
+		((total_heads++))
 	else
 		echo "TAILS IS WINNER"
+		((total_tails++))
 	fi
 	((counter++))
 done
+
+echo "HEADS WON " $total_heads " times"
+echo "TAILS WON " $total_tails " times"
