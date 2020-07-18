c9778d8ea91981898a2e5839b644ed7328f5776e
Sanjana-cell <sanjanakr97@gmail.com>
2020-07-18 08:18:11 +0530

    Refactor: modified code to loop through Multiple times of flipping a coin and show number of times head and tail has won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,18 @@
-coin=$(($RANDOM%2))
-if [ $coin -eq 1 ]
-then
-	echo "HEADS IS WINNER"
-else
-	echo "TAILS IS WINNER"
-fi
+#!/bin/bash -x
 
+#varibles
+coin=0
+counter=0
+
+read -p "Enter how many times to flip the coin " flip
+while [ $counter -le $flip ]
+do
+	coin=$(($RANDOM%2))
+	if [ $coin -eq 1 ]
+	then
+		echo "HEADS IS WINNER"
+	else
+		echo "TAILS IS WINNER"
+	fi
+	((counter++))
+done
